814d8dc3b58cc08717f50bc60120a5c172aaeab2
Kyle Pu <kylez.pu@gmail.com>
2018-09-06 22:23:32 -0700

    First commit

---
+++ app/src/main/java/com/example/zpu/webviewapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.zpu.webviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl("http://google.com/");
+
+    }
+}
